6a151a8ef44069f1f1ad1aecfed875997fd56f9b
Yevgen Nabokov <55064156+YevgenNabokov@users.noreply.github.com>
2020-04-18 01:27:49 +0200

    Added plugin signature and outputs initialization.

--- DemoPlugin/PrintNicePlugin/Plugin.cs
+++ DemoPlugin/PrintNicePlugin/Plugin.cs
@@ -1,5 +1,7 @@
-﻿using ForgedOnce.Core.Interfaces;
+﻿using ForgedOnce.Core;
+using ForgedOnce.Core.Interfaces;
 using ForgedOnce.Core.Metadata.Interfaces;
+using ForgedOnce.Core.Plugins;
 using ForgedOnce.CSharp;
 using System;
 using System.Collections.Generic;
@@ -8,9 +10,26 @@ namespace PrintNicePlugin
 {
     public class Plugin : CodeGenerationFromCSharpPlugin<Settings, Parameters>
     {
+        public const string OutStreamName = "PassThrough";
+
+        private ICodeStream passThroughStream;
+
+        public Plugin()
+        {
+            this.Signature = new PluginSignature()
+            {
+                Id = "4DAB3B44-C7C1-4578-95EE-8252D873BB83",
+                InputLanguage = Languages.CSharp,
+                Name = typeof(Plugin).FullName
+            };
+        }
+
         protected override List<ICodeStream> CreateOutputs(ICodeStreamFactory codeStreamFactory)
         {
-            throw new NotImplementedException();
+            List<ICodeStream> result = new List<ICodeStream>();
+            this.passThroughStream = codeStreamFactory.CreateCodeStream(Languages.CSharp, OutStreamName);
+            result.Add(this.passThroughStream);
+            return result;
         }
 
         protected override void Implementation(CodeFileCSharp input, Parameters inputParameters, IMetadataRecorder metadataRecorder, ILogger logger)
