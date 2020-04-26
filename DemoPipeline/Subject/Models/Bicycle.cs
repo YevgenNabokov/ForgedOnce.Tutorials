318040266624b60a9fab84e95dd26a7de82a3aa0
Yevgen Nabokov <55064156+YevgenNabokov@users.noreply.github.com>
2020-04-26 22:52:14 +0200

    Initial version of the tutorial pipeline.

---
+++ DemoPipeline/Subject/Models/Bicycle.cs
@@ -0,0 +1,11 @@
+﻿namespace Subject.Models
+{
+    public class Bicycle
+    {
+        public string Name { get; set; }
+
+        public Wheel FrontWheel { get; set; }
+
+        public Wheel RearWheel { get; set; }
+    }
+}
---
+++ DemoPipeline/Subject/Models/Wheel.cs
@@ -0,0 +1,11 @@
+﻿namespace Subject.Models
+{
+    public class Wheel
+    {
+        public int NumberOfSpokes { get; set; }
+
+        public decimal RimSizeInInches { get; set; }
+
+        public decimal TyreWidth { get; set; }
+    }
+}
